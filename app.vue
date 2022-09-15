9c7acaf78eb7e7e67736d2195ecc41ba9f8a3684
Lucas Simines <lucas@fmcx.digital>
2022-09-15 17:56:26 -0300

    build:

---
+++ app.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+const { data } = await useFetch('https://worldtimeapi.org/api/timezone/America/Sao_Paulo');
+</script>
+
+<template>
+  <div>
+    <div>{{ data.datetime }}</div>
+  </div>
+</template>
